ea1a2ad99d600a320d0b1fbc2389278ddde64c0f
Thomas Gaspar <thomaserikgaspar@hotmail.com>
2024-08-12 16:41:11 -0300

    feat(user): Adding the user test.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
-const app = require('express')();
+const app = require("express")();
 
-app.get('/', (req, res) => {
-    res.status(200).send();
+app.get("/", (req, res) => {
+  res.status(200).send();
+});
+
+app.get("/users", (req, res) => {
+  const user = [{ name: "John Doe", mail: "johndoe@mail.com" }];
+  res.status(200).json(user);
 });
 
 module.exports = app;
