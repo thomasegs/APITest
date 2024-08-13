37d05693c87f417a6f1a3c5301fac6948adae1e0
Thomas Gaspar <thomaserikgaspar@hotmail.com>
2024-08-13 14:27:30 -0300

    feat(validate password): Adding a way to validate empty password, add routines to test that.

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -3,37 +3,53 @@ const request = require("supertest");
 const app = require("../../src/app");
 
 test("Deve listar todos os usuários", () => {
-  return request(app).get('/users')
+  return request(app)
+    .get("/users")
     .then((res) => {
       expect(res.status).toBe(200);
       expect(res.body.length).toBeGreaterThan(0);
     });
 });
 
-test('Deve inserir usuário com sucesso', () => {
-  const mail = `${Date.now()}@mail.com`  
-  return request(app).post('/users')
-        .send({ name: 'Walter Mitty', mail: `${mail}`, passwd: '123456'})
-        .then((res) => {
-            expect(res.status).toBe(201);
-            expect(res.body.name).toBe('Walter Mitty');
-        })
+test("Deve inserir usuário com sucesso", () => {
+  const mail = `${Date.now()}@mail.com`;
+  return request(app)
+    .post("/users")
+    .send({ name: "Walter Mitty", mail: `${mail}`, passwd: "123456" })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe("Walter Mitty");
+    });
 });
 
-test('Não deve inserir usuários sem nome', () => {
-  const mail = `${Date.now()}@mail.com`
-  return request(app).post('/users')
-    .send({mail: `${mail}`, passwd:`123456`})
+test("Não deve inserir usuários sem nome", () => {
+  const mail = `${Date.now()}@mail.com`;
+  return request(app)
+    .post("/users")
+    .send({ mail: `${mail}`, passwd: `123456` })
     .then((res) => {
       expect(res.status).toBe(400);
-      expect(res.body.error).toBe('Nome é um atributo obrigatório')
-    })
+      expect(res.body.error).toBe("Nome é um atributo obrigatório");
+    });
 });
 
-test('Não deve inserir usuário sem email', async () => {
-  const result = await request(app).post('/users')
-    .send({name: 'Walter Mitty', passwd: '123456'});
-    expect(result.status).toBe(400);
-    expect(result.body.error).toBe('Email é um atributo obrigatório')
-    
-})
+test("Não deve inserir usuário sem email", async () => {
+  const result = await request(app)
+    .post("/users")
+    .send({ name: "Walter Mitty", passwd: "123456" });
+  expect(result.status).toBe(400);
+  expect(result.body.error).toBe("Email é um atributo obrigatório");
+});
+
+test("Não deve inserir usuário sem senha", (done) => {
+  const mail = `${Date.now()}@mail.com`;
+  request(app)
+    .post("/users")
+    .send({ name: "Walter Mitty", mail: `${mail}` })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe("Senha é um atributo obrigatório");
+      done();
+    })
+    .catch((err) => done.fail(err));
+});
