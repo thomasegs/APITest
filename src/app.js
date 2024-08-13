8d42fdc42843963a9653d79c85b49761cac5ea5e
Thomas Gaspar <thomaserikgaspar@hotmail.com>
2024-08-12 21:11:26 -0300

    feat(users): Adding the post verb to users.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const app = require("express")();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   res.status(200).send();
@@ -8,5 +11,9 @@ app.get("/users", (req, res) => {
   const user = [{ name: "John Doe", mail: "johndoe@mail.com" }];
   res.status(200).json(user);
 });
+
+app.post('/users', (req, res) => {
+        res.status(201).json(req.body);
+})
 
 module.exports = app;
